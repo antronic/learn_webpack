322f1b2c420e834fe0185cc5eeda68bbd333aeb5
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-04-21 21:46:06 +0700

    new example

--- webpack2/ex02/src/index.js
+++ webpack2/ex02/src/index.js
@@ -1,3 +1,11 @@
-require('../style.css')
+// require('../style.css')
 
-console.log('happy css')
+// console.log('happy css')
+
+const func1 = (() => {
+	return 'test'
+}) 
+
+const var1 = 'abc'
+console.log(var1)
+console.log(func1())
